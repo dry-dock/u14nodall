78fc1d04d2e8c53300fdeea7d661227638bf8b44
Ubuntu <ubuntu@ip-80-0-0-44.ec2.internal>
2018-05-21 07:54:27 +0000

    update the node versions

--- version/8_9_4.sh
+++ version/8_9_4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-NODE_VERSION=8.9.4
+NODE_VERSION=8.11.2
 NPM_VERSION=5.6.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
-
+nvm alias default 8.11.2
