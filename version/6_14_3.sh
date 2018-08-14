d676ef03c0b31a886e70bf586ddb322db9bf8994
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-08-14 15:42:05 +0530

    Update 6_14_3.sh

--- version/6_14_3.sh
+++ version/6_14_3.sh
@@ -3,5 +3,5 @@
 NODE_VERSION=6.14.3
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
-nvm use $NODE_VERSION && npm install npm@latest -g
+nvm use $NODE_VERSION && npm install npm@6.3.0 -g
 
